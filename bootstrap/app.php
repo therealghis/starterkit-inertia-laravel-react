a2d183b61afd944425065db51e36e88dfff31f53
Pietro Ghislieri <pietro.ghislieri@shellrent.com>
2026-03-21 11:47:14 +0100

    Spostamento della schedulazione del comando `security:daily-scan` dalla configurazione di bootstrap ai file delle route console

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\SetLocale;
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,12 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withSchedule(function (Schedule $schedule): void {
-        $schedule
-            ->command('security:daily-scan')
-            ->dailyAt('02:00')
-            ->withoutOverlapping();
-    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
