52f26a930e22e3d368cdc86b1555798d489582e1
Pietro Ghislieri <pietro.ghislieri@shellrent.com>
2026-05-19 16:03:01 +0200

    Corretto `enum MergeAcquisitionType`: aggiunto tipo esplicito `string` alle costanti.

--- app/Enums/MergeAcquisitionType.php
+++ app/Enums/MergeAcquisitionType.php
@@ -3,6 +3,6 @@
 namespace App\Enums;
 
 enum MergeAcquisitionType: string {
-    public const BUY_SIDE = 'BUY_SIDE';
-    public const SELL_SIDE = 'SELL_SIDE';
+    public const string BUY_SIDE = 'BUY_SIDE';
+    public const string SELL_SIDE = 'SELL_SIDE';
 }
