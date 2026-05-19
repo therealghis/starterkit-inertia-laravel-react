97ec260f3c8cd0b1c7b2ce9ebfef111f00914c61
Pietro Ghislieri <pietro.ghislieri@shellrent.com>
2026-05-19 14:30:19 +0200

    Rimozione della pagina demo "Server Data Table" e della relativa route.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -22,10 +22,4 @@ class ExampleTest extends TestCase {
 
         $response->assertRedirect(route('dashboard'));
     }
-
-    public function test_server_data_table_demo_route_is_not_registered() {
-        $response = $this->get('/components/server-data-table-demo');
-
-        $response->assertNotFound();
-    }
 }
